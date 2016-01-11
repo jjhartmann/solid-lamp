59d45c26de7ce2a74a8d6b3d3fadd3fd233ad6ea
jhartman <jhartman@sfu.ca>
2016-01-11 00:24:09 -0800

    Add method implementations of ERresolution Class to .cpp

--- EResolution.cpp
+++ EResolution.cpp
@@ -9,3 +9,24 @@
 #include "rapidjson/stringbuffer.h"
 
 #include "ListingObject.h"
+
+
+// Builds and parses the product file into a data structure
+//
+// IN: in_path      takes a path to the product data file.
+// IN: in_path      takes a path to the listings data file.
+//
+EResolution::EResolution(string in_ListingPath, string in_ProductPath)
+{
+
+}
+
+
+
+// Write the entity resolution result to JSON format.
+//
+// IN: in_FileName      the filename of the docuemtn to write.
+void EResolution::writeJSON()
+{
+
+}
