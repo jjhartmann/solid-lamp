e5f3b07ae7f5099fb073d82fd0f91e9f2710f78b
jhartman <jhartman@sfu.ca>
2016-01-12 13:23:25 -0800

    Add intital test for the Entity resolution class

--- main.cpp
+++ main.cpp
@@ -4,25 +4,35 @@
 #include "rapidjson/stringbuffer.h"
 #include <iostream>
 
+#include "EResolution.h"
+
 using namespace rapidjson;
 using namespace std;
 
 int main() {
-    // 1. Parse a JSON string into DOM.
-    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-    Document d;
-    d.Parse(json);
+//    // 1. Parse a JSON string into DOM.
+//    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+//    Document d;
+//    d.Parse(json);
+//
+//    // 2. Modify it by DOM.
+//    Value& s = d["stars"];
+//    s.SetInt(s.GetInt() + 1);
+//
+//    // 3. Stringify the DOM
+//    StringBuffer buffer;
+//    Writer<StringBuffer> writer(buffer);
+//    d.Accept(writer);
+//
+//    // Output {"project":"rapidjson","stars":11}
+//    std::cout << buffer.GetString() << std::endl;
 
-    // 2. Modify it by DOM.
-    Value& s = d["stars"];
-    s.SetInt(s.GetInt() + 1);
 
-    // 3. Stringify the DOM
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    d.Accept(writer);
+    string fileProduct = "./data/products.txt";
+    string fileListing = "./data/listings.txt";
 
-    // Output {"project":"rapidjson","stars":11}
-    std::cout << buffer.GetString() << std::endl;
+
+    EResolution resolve(fileListing, fileProduct);
+
     return 0;
 }
