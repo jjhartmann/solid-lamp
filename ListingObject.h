afa41b5d29892585f1ad906ff4a713fac11398d1
jhartman <jhartman@sfu.ca>
2016-01-11 22:21:49 -0800

    Add methods to the class ListingObject
    Class will hold and organize all listings based on the manufacturer type.

--- ListingObject.h
+++ ListingObject.h
@@ -5,9 +5,44 @@
 #ifndef SORTABLECHALLENGEREPO_LISTINGOBJECT_H
 #define SORTABLECHALLENGEREPO_LISTINGOBJECT_H
 
+#include <unordered_set>
+
+#include "ListingManufacturer.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+
+using namespace std;
 
 class ListingObject
 {
+    ListingObject();
+    ~ListingObject();
+
+
+    // Add a document file to the listings object.
+    //
+    // IN: in_d         The json object to be added.
+    void add(rapidjson::Document in_d);
+
+    // Reset the counter for the Manufacturer to start at zero.
+    void resetManufacturerItr();
+
+    // Iterate to the next manufacurer in the list.
+    void getManufacturerNext();
+
+    // Returns the number of Manufacturers.
+    int getManufacturerCount();
+
+    // Get the value of a Member in the document
+    //
+    // IN: in_str           The name of the member to get.
+    rapidjson::Value& operator[] (string str);
+
+
+private:
+    // Private Variables
+    unordered_set<ListingManufacturer*> mManufacturerList;
+
 
 };
 
