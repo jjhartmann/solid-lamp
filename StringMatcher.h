e363da6a9fb672653ed2efd25e879bd41c1a38f5
jhartman <jhartman@sfu.ca>
2016-01-11 23:16:57 -0800

    Add Methods and Functors
    Add methods in the StringMatcher class
    Add Functors that the stringmatcher will used based on diferent cases.

--- StringMatcher.h
+++ StringMatcher.h
@@ -5,11 +5,40 @@
 #ifndef SORTABLECHALLENGEREPO_STRINGMATCHER_H
 #define SORTABLECHALLENGEREPO_STRINGMATCHER_H
 
+#include <string>
 
+using namespace std;
+
+template <typename FuncType>
 class StringMatcher
 {
+public:
+    StringMatcher(FuncType in_matcher);
+    bool match(string str1, string str2);
 
+private:
+    FuncType mMatcher;
 };
 
 
 #endif //SORTABLECHALLENGEREPO_STRINGMATCHER_H
+
+
+///////////////////////////////////////////////////////////////
+// Functor: Manufacturer Matching
+///////////////////////////////////////////////////////////////
+class ManufacturerMatcher
+{
+public:
+    bool operator()(string str1, string str2);
+};
+
+
+///////////////////////////////////////////////////////////////
+// Functor: Manufacturer Matching
+///////////////////////////////////////////////////////////////
+class ProductMatcher
+{
+public:
+    bool operator()(string str1, string str2);
+};
