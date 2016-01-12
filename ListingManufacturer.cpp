43110a53e8d0abc50250a4b700d1d59c9da712b0
jhartman <jhartman@sfu.ca>
2016-01-12 11:47:00 -0800

    ADD: impl skelton for ListingManufacturer

--- ListingManufacturer.cpp
+++ ListingManufacturer.cpp
@@ -3,3 +3,38 @@
 ///////////////////////////////////////////////////////////////
 
 #include "ListingManufacturer.h"
+
+void ListingManufacturer::add(rapidjson::Document *in_d)
+{
+
+}
+
+ListingManufacturer::ListingManufacturer(string in_manName)
+{
+
+}
+
+ListingManufacturer::~ListingManufacturer()
+{
+
+}
+
+rapidjson::Value &ListingManufacturer::operator[](string in_memberName)
+{
+
+}
+
+void ListingManufacturer::resetDocumentItr()
+{
+
+}
+
+void ListingManufacturer::operator++()
+{
+
+}
+
+string ListingManufacturer::getName()
+{
+
+}
